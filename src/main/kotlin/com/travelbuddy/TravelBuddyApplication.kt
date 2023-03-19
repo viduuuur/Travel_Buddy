d0f127d30b174512758b6a38e831a12a572ecc5e
Vidur Sachdeva <viduresachdeva94@gmail.com>
2023-03-19 17:12:57 +0530

    init

---
+++ src/main/kotlin/com/travelbuddy/TravelBuddyApplication.kt
@@ -0,0 +1,13 @@
+package com.travelbuddy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelBuddyApplication
+
+fun main(args: Array<String>) {
+	runApplication<TravelBuddyApplication>(*args)
+}
+
+
